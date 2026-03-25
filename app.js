21db5646ba7cb91be2000b29f1f425d7d99ba925
DEVESH TIWARI <deveshups@gmail.com>
2026-03-25 11:27:06 +0530

    second commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const io = socket(server);
 
 const chess = new Chess();
 let players = {};
-let currentPlayer = "W";
+let currentPlayer = "w";
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -22,8 +22,42 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", function (uniquesocket) {
-  uniquesocket.on("samosa", function () {
-    io.emit("chatni");
+  if (!players.white) {
+    players.white = uniquesocket.id;
+    uniquesocket.emit("playerRole", "w");
+  } else if (!players.black) {
+    players.black = uniquesocket.id;
+    uniquesocket.emit("playerRole", "b");
+  } else {
+    uniquesocket.emit("spectatorRole");
+  }
+
+  uniquesocket.on("disconnect", function () {
+    if (uniquesocket.id === players.white) {
+      delete players.white;
+    } else if (uniquesocket.id === players.black) {
+      delete players.black;
+    }
+  });
+
+  uniquesocket.io("move", (move) => {
+    try {
+      if (chess.turn() === "w" && uniquesocket.id !== players.white) return;
+      if (chess.turn() === "b" && uniquesocket.id !== players.black) return;
+
+      const result = chess.move(move);
+      if (result) {
+        currentPlayer = chess.turn();
+        io.emit("move", move);
+        io.emit("boardState", chess.fen());
+      } else {
+        console.log("Invalid move : ", move);
+        uniquesocket.emit("invalidMove", move);
+      }
+    } catch (error) {
+      console.log(error);
+      uniquesocket.emit("Invalid move :", move);
+    }
   });
 });
 
